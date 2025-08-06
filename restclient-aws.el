c6f9616c04c4cdec7c1dad5267615a3af6e43ab5
kevwargo <jarasz.kevwargo@gmail.com>
2025-08-06 21:54:47 +0200

    Add the ability to build the signer binary

--- restclient-aws.el
+++ restclient-aws.el
@@ -40,22 +40,34 @@
 (defvar restclient-aws--signer-exec
   (expand-file-name "signer" restclient-aws--cache-dir))
 
+(defvar restclient-aws--signer-src-dir nil)
+(if load-file-name
+    (setq restclient-aws--signer-src-dir
+          (expand-file-name
+           "signer"
+           (file-name-directory (file-truename (file-name-with-extension
+                                                (file-name-sans-extension load-file-name)
+                                                "el"))))))
+
 (defun restclient-aws--sign (profile region service request)
-  (let ((client (or (restclient-aws--connect)
-                    (restclient-aws--reconnect))))
-    (process-put client :request request)
-    (process-send-string client
-                         (json-serialize
-                          `((profile . ,profile)
-                            (region . ,region)
-                            (service . ,service)
-                            (method . ,(plist-get request :method))
-                            (url . ,(plist-get request :url))
-                            (headers . ,(mapcar (lambda (h)
-                                                  (cons (intern (car h)) (cdr h)))
-                                                (plist-get request :headers)))
-                            (body . ,(plist-get request :body)))
-                          :null-object nil))))
+  (let ((init-client-fn (lambda (client)
+                   (process-put client :request request)
+                   (process-send-string client
+                                        (json-serialize
+                                         `((profile . ,profile)
+                                           (region . ,region)
+                                           (service . ,service)
+                                           (method . ,(plist-get request :method))
+                                           (url . ,(plist-get request :url))
+                                           (headers . ,(mapcar (lambda (h)
+                                                                 (cons (intern (car h)) (cdr h)))
+                                                               (plist-get request :headers)))
+                                           (body . ,(plist-get request :body)))
+                                         :null-object nil))))
+        (client (restclient-aws--connect)))
+    (if client
+        (funcall init-client-fn client)
+      (restclient-aws--reconnect init-client-fn))))
 
 (defun restclient-aws--connect (&optional keep-unbound-socket)
   (restclient-aws--log "Connecting to %s ..." restclient-aws--signer-socket)
@@ -79,25 +91,60 @@
                  (delete-file restclient-aws--signer-socket))))
        (signal (car err) (cdr err))))))
 
-(defun restclient-aws--reconnect ()
-  (unless (process-live-p restclient-aws--signer-proc)
-    (setq restclient-aws--signer-proc
-          (make-process
-           :name "aws-signer"
-           :command (list restclient-aws--signer-exec restclient-aws--signer-socket)
-           :buffer "*aws-signer*"))
-    (restclient-aws--log "Started %s with PID %d"
-                         restclient-aws--signer-exec
-                         (process-id restclient-aws--signer-proc)))
-  (let ((client nil)
-        (ts (current-time)))
-    (while (and (not client)
-                (process-live-p restclient-aws--signer-proc)
-                (time-less-p (time-since ts) 1))
-      (sleep-for 0.1)
-      (setq client (restclient-aws--connect t)))
-    (or client
-        (error "Could not reconnect to %s" restclient-aws--signer-socket))))
+(defun restclient-aws--reconnect (init-client-fn)
+  (restclient-aws--maybe-start-signer
+   (lambda ()
+     (let ((client nil)
+           (ts (current-time)))
+       (while (and (not client)
+                   (process-live-p restclient-aws--signer-proc)
+                   (time-less-p (time-since ts) 1))
+         (sleep-for 0.1)
+         (setq client (restclient-aws--connect t)))
+       (if client
+           (funcall init-client-fn client)
+         (error "Could not reconnect to %s" restclient-aws--signer-socket))))))
+
+(defun restclient-aws--maybe-start-signer (after-signer-start)
+  (if (process-live-p restclient-aws--signer-proc)
+      (funcall after-signer-start)
+    (if (file-executable-p restclient-aws--signer-exec)
+        (progn
+          (restclient-aws--start-signer)
+          (funcall after-signer-start))
+      (and (y-or-n-p "Request signer for restclient-aws does not seem to be installed. Try to build from source? ")
+           (or (not (file-exists-p restclient-aws--signer-exec))
+               (y-or-n-p (format "File %s already exists. Overwrite? "
+                                 restclient-aws--signer-exec)))
+           (let ((default-directory restclient-aws--signer-src-dir))
+             (make-process
+              :name "restclient-aws-signer-build"
+              :command `("go" "build" "-o" ,restclient-aws--signer-exec "main.go")
+              :buffer (with-current-buffer
+                          (generate-new-buffer "*restclient-aws-signer-build*")
+                        (keymap-local-set "q" 'kill-current-buffer)
+                        (keymap-local-set "k" 'kill-current-buffer)
+                        (current-buffer))
+              :sentinel (lambda (proc event)
+                          (if (equal event "finished\n")
+                              (progn
+                                (kill-buffer (process-buffer proc))
+                                (restclient-aws--start-signer)
+                                (funcall after-signer-start))
+                            (with-current-buffer (process-buffer proc)
+                              (goto-char (point-max))
+                              (insert (propertize event 'face 'font-lock-warning-face))
+                              (pop-to-buffer (current-buffer)))))))))))
+
+(defun restclient-aws--start-signer ()
+  (setq restclient-aws--signer-proc
+        (make-process
+         :name "aws-signer"
+         :command (list restclient-aws--signer-exec restclient-aws--signer-socket)
+         :buffer "*aws-signer*"))
+  (restclient-aws--log "Started %s with PID %d"
+                       restclient-aws--signer-exec
+                       (process-id restclient-aws--signer-proc)))
 
 (defun restclient-aws--client-sentinel (p event)
   (pcase event
@@ -139,10 +186,13 @@
      (restclient-aws--complete-request (plist-put (process-get proc :request)
                                                   :headers
                                                   (alist-get 'headers resp))))
-    ("error"
-     (restclient-aws--log "%S: ERROR: %s" proc (alist-get 'err-msg resp)))
     (_
-     (restclient-aws--log "%S: unexpected %S" proc resp))))
+     (let* ((err (alist-get 'err-msg resp))
+            (err (if err
+                     (format "ERROR: %s" err)
+                   (format "unexpected response %S" resp))))
+       (restclient-aws--log "%S: ERROR: %s" proc err)
+       (message "%S: ERROR: %s" proc err)))))
 
 (defun restclient-aws--read-mfa (resp)
   (let* ((profile (format "profile %s" (alist-get 'profile resp)))
